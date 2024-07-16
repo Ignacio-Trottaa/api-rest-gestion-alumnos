e77dda6e9c423bcc0e240cd42f8d37af9400eba6
nacho <nachotrotta81@gmail.com>
2024-07-15 22:37:34 -0300

    first commit

---
+++ src/main/java/edu/campus/campusLeopoldoMarechal/CampusLeopoldoMarechalApplication.java
@@ -0,0 +1,13 @@
+package edu.campus.campusLeopoldoMarechal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusLeopoldoMarechalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusLeopoldoMarechalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/campus/campusLeopoldoMarechal/CampusLeopoldoMarechalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.campus.campusLeopoldoMarechal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusLeopoldoMarechalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
