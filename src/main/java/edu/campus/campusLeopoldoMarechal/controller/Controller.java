b2c5eb7f71441a007c4eedff9e22bd381880391a
nacho <nachotrotta81@gmail.com>
2024-07-29 14:57:20 -0300

    arquitectura multicapas: se agrego los respectivos paquetes que conforman la arquitectura multicapas

---
+++ src/main/java/edu/campus/campusLeopoldoMarechal/controller/Controller.java
@@ -0,0 +1,17 @@
+package edu.campus.campusLeopoldoMarechal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class Controller {
+
+    @GetMapping("test/")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+
+}
